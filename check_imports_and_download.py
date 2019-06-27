c614eb805e2688f65961db4dbf7dcaff79329687
Dougal J. Sutherland <dougal@gmail.com>
2019-06-27 14:06:24 +0200

    Built for 38cb4f96 (update setup instructions)

---
+++ check_imports_and_download.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os
+
+import matplotlib as mpl
+import numpy as np
+import seaborn as sns
+import sklearn
+import torch
+import torchvision
+import tqdm
+
+import ds3_support
+
+torchvision.datasets.MNIST(root='data', download=True)
+ds3_support.CombinedOmniglot(root='data', download=True)
+
+assert os.path.exist('data/transcripts.tar.bz2')
+
+print("Okay, you're good to go!")
