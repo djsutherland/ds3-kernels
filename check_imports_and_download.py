b0fe90eabc90bc30b9693cd1b4d3cb9e997cc539
Dougal J. Sutherland <dougal@gmail.com>
2019-06-28 14:16:56 +0200

    Built for 97ce0740 (fix typo in check script)

--- check_imports_and_download.py
+++ check_imports_and_download.py
@@ -14,6 +14,6 @@ import ds3_support
 torchvision.datasets.MNIST(root='data', download=True)
 ds3_support.CombinedOmniglot(root='data', download=True)
 
-assert os.path.exist('data/transcripts.tar.bz2')
+assert os.path.exists('data/transcripts.tar.bz2')
 
 print("Okay, you're good to go!")
